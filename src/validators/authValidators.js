667ddfcb13e97ee70ad35210668eb224f23a47d3
Dominic Jerryemmanuel <Jerryemmanuelolisa@gmail.com>
2025-06-18 02:20:49 +0100

    Update authValidators.js

--- src/validators/authValidators.js
+++ src/validators/authValidators.js
@@ -5,8 +5,7 @@ export const registerValidation = [
   body("username")
     .escape()
     .trim()
-    .notEmpty()
-    .withMessage("Username is required")
+    .optional()
     .isLength({ min: 3, max: 30 })
     .withMessage("Username must be between 3 and 30 characters"),
   body("email")
